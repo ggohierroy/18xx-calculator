9aa979b5c9c511d5d6f984dabd030db5d28dffd8
Gabriel Gohier-Roy <ggohierroy@hotmail.com>
2022-03-11 15:36:59 -0500

    add material ui

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Button from '@mui/material/Button';
+
+const Test: NextPage = () => {
+    return (
+        <Button variant="contained">Hello</Button>
+    )
+}
+
+export default Test
